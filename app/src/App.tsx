75acd1378cc0cfe15b6b79b97676cda617232d93
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-03-23 18:44:07 +0900

    add @babel/plugin-proposal-explicit-resource-management

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { MyComponent } from './components/MyComponent'
 
 function App() {
 
@@ -17,6 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
      
+     <MyComponent />
     </>
   )
 }
